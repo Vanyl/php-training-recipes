5e762fd2308f3432b35b9850b11e67c3b137a7ea
vanyl <yavannaperez@hotmail.com>
2025-06-06 16:50:19 +0200

    login page logic separation + sessions added

--- login.php
+++ login.php
@@ -1,41 +1,10 @@
-<?php
-$postData = $_POST;
-
-if (isset($postData['email']) && isset($postData['password'])) {
-    if(!filter_var($postData['email'], FILTER_VALIDATE_EMAIL)) {
-        $errorMessage  = 'L\'email doit être valide.';
-    } else {
-        foreach ($users as $user)  {
-            if(
-                $user['email'] === $postData['email']
-                && $user['password'] === $postData['password']
-            ) {
-                // $isLogged = true;
-                // $loggedUser = $postData['email']; //['email' => $user['email']];
-                $_SESSION['LOGGED_USER'] = $user['email'];
-            } // else {
-            //     $errorMessage = 'Email ou mot de passe incorrect.';
-            // }
-        }
-
-        if(!isset($SESSION['LOGGED_USER'])){
-            $errorMessage = sprintf(
-                'Aucun utilisateur trouvé avec les informations envoyées : (%s%s)',
-                $postData['email'],
-                strip_tags($postData['password'])
-            );
-        }
-    }
-}
-?>
-
-
-<?php if(!isset($SESSION['LOGGED_USER'])): ?> <!-- if(!isset($loggedUser)) or !$isLogged -->
-    <form action="index.php" method="POST">
+<?php if(!isset($_SESSION['LOGGED_USER'])): ?>
+    <form action="submit_login.php" method="POST">
 <!-- if not logged succesfully show error message -->
-        <?php if(isset($errorMessage)) : ?>
+        <?php if(isset($_SESSION['LOGIN_ERROR_MESSAGE'])) : ?>
             <div class="alert alert-danger" role="alert">
-                <?php echo $errorMessage; ?>
+                <?php echo $_SESSION['LOGIN_ERROR_MESSAGE']; 
+                unset($_SESSION['LOGIN_ERROR_MESSAGE']); ?>
             </div>
         <?php endif ?>
 
@@ -52,7 +21,7 @@ if (isset($postData['email']) && isset($postData['password'])) {
 <!--if logged successfuly show success message  -->
 <?php else : ?>
     <div class="alert alert-success" role="alert">
-        <p>Hello <?php echo $SESSION['LOGGED_USER']; ?> and welcome!</p>
+        <p>Hello <?php echo $_SESSION['LOGGED_USER']; ?> and welcome!</p>
     </div>
 <?php endif ?>
 

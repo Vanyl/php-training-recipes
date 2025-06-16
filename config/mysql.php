2588db9ab85ef6e09f168706806552e7abfe60bc
vanyl <yavannaperez@hotmail.com>
2025-06-16 22:23:41 +0200

    mysql files with variables

---
+++ config/mysql.php
@@ -0,0 +1,7 @@
+<?php 
+    const MYSQL_HOST = 'localhost';
+    const MYSQL_PORT = 3306; // Default MySQL port
+    const MYSQL_DB_NAME = 'partage_de_recettes';
+    const MYSQL_USERNAME = 'root';
+    const MYSQL_PASSWORD = '';
+?>
